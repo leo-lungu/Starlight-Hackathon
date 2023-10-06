732a02f0cd7c4d4985c8d2a9335d829b3170ae14
Al Adnan Uddin <shuhanuddin6@gmail.com>
2023-10-06 23:43:11 +0100

    test

--- faceTracking.py
+++ faceTracking.py
@@ -2,45 +2,37 @@ import cv2
 from deepface import DeepFace
 import pygame
 
-<<<<<<< HEAD
-# Initialize mixer
-=======
-# Initialize pygame mixer 
->>>>>>> 1aef3027ce8347f8c1778c1547c74cac483b3c6a
+#Initialize mixer
 pygame.mixer.init()
 
 # Load audio files
-happy_sound = pygame.mixer.Sound('music/happy.mp3')
-sad_sound = pygame.mixer.Sound('music/sad.mp3')
+happy_sound = pygame.mixer.Sound('./music/happy.mp3')
+sad_sound = pygame.mixer.Sound('./music/sad.mp3')
+Angry_sound = "no sound"
+Disgust_sound = "no sound"
 
 # Flags to track if sounds are already playing
 is_happy_playing = False
 is_sad_playing = False
 
-# Initialize webcam
-cap = cv2.VideoCapture(0)  
+# Initialize the webcam
+cap = cv2.VideoCapture(0)  # 0 for the default webcam
 
 while True:
-    # Capture frame
+    # Capture frame-by-frame
     ret, frame = cap.read()
 
-<<<<<<< HEAD
     # Perform emotion analysis
     try:
         result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
 
         # If result is a non-empty list, proceed
-=======
-    # Detect emotion
-    try:
-        result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
-            
->>>>>>> 1aef3027ce8347f8c1778c1547c74cac483b3c6a
         if isinstance(result, list) and len(result) > 0:
             emotion = result[0].get('dominant_emotion', "N/A")
 
             if emotion == "happy":
                 if not is_happy_playing:
+                    
                     pygame.mixer.stop()
                     happy_sound.play()
                     is_happy_playing = True
@@ -60,10 +52,9 @@ while True:
             emotion = "N/A"
 
     except Exception as e:
-        print(f"Exception: {e}")
+        print(f"An exception occurred: {e}")
         emotion = "Error"
 
-<<<<<<< HEAD
     # Display the emotion on the frame
     cv2.putText(frame, emotion, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
@@ -75,24 +66,5 @@ while True:
         break
 
 # Release the webcam and destroy all OpenCV windows
-=======
-    # Play audio based on emotion
-    if emotion == "happy":
-        happy_sound.play()
-    elif emotion == "sad":
-        sad_sound.play()
-
-    # Display emotion text on frame 
-    cv2.putText(frame, emotion, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        
-    # Display frame
-    cv2.imshow('Emotion Detector', frame)
-
-    # Break loop with 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-        
-# Release resources
->>>>>>> 1aef3027ce8347f8c1778c1547c74cac483b3c6a
 cap.release()
 cv2.destroyAllWindows()
