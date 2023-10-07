a2ef65bcb223aa5186aa03fd7381e3be2f53f2c7
Al Adnan Uddin <shuhanuddin6@gmail.com>
2023-10-07 22:02:41 +0100

    Camera toggle added its hidden by default

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import cv2
 import streamlit as st
 import queue
 import base64
+import os
+import random
 
-# Load happy and sad mp3 files
-happy = open("music/happy.mp3", "rb").read()
-sad = open("music/sad.mp3", "rb").read()
+# Function to load a random mp3 file from a given folder
+def load_random_song(folder):
+    files = [f for f in os.listdir(folder) if f.endswith('.mp3')]
+    if not files:
+        return None
+    random_file = random.choice(files)
+    return open(os.path.join(folder, random_file), "rb").read()
 
 # Initialize session state if not already done
 if "emotion" not in st.session_state:
@@ -14,6 +20,7 @@ if "emotion" not in st.session_state:
     st.session_state.emotions = queue.Queue()
     st.session_state.playing = None
     st.session_state.audio_player = None
+    st.session_state.show_camera = False
 
 # Import DeepFace for emotion recognition
 with st.spinner("Importing DeepFace..."):
@@ -49,7 +56,17 @@ detected = col1.empty()
 current = col2.empty()
 playing = col3.empty()
 audio = st.markdown("", unsafe_allow_html=True)
-FRAME_WINDOW = st.image([])
+
+
+# Add button to toggle camera visibility
+if st.button("Toggle Camera"):
+    st.session_state.show_camera = not st.session_state.show_camera
+
+# Only create the image placeholder if the camera is enabled
+if st.session_state.show_camera:
+    FRAME_WINDOW = st.image([])  # Initialize image placeholder
+else:
+    FRAME_WINDOW = None  # Set to None to skip updating the frame
 
 # Start the camera
 with st.spinner("Accessing Camera..."):
@@ -66,31 +83,25 @@ while True:
     if ret:
         # Convert the frame color to RGB
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(frame, use_column_width=True)
+
+        # Only update the frame if the camera is enabled
+        if st.session_state.show_camera and FRAME_WINDOW is not None:
+            FRAME_WINDOW.image(frame, use_column_width=True)
 
         # Update session state with detected emotion
         st.session_state.emotion = get_emotion(frame)
         current_emotion = get_current_emotion()
 
+        # Update current emotion if it's changed and is not None
+        if current_emotion and (current_emotion != st.session_state.playing):
+            st.session_state.playing = current_emotion
+            
+            # Load random song based on current emotion 
+            song_data = load_random_song(f"music/{current_emotion}")
+            if song_data:
+                encoded_song = base64.b64encode(song_data).decode('utf-8')
+                audio.markdown(f'<audio style="width: 100%;" src="data:audio/mp3;base64,{encoded_song}" autoplay controls></audio>', unsafe_allow_html=True)
+
         detected.write(f"Detected emotion: `{st.session_state.emotion}`")
         current.write(f"Current emotion: `{str(current_emotion)}`")
         playing.write(f"Playing: `{str(st.session_state.playing)}`")
-
-        # Update current emotion if it's changed
-        if current_emotion != st.session_state.emotion:
-            if current_emotion == "happy":
-                st.session_state.emotion = current_emotion
-            elif current_emotion == "sad":
-                st.session_state.emotion = current_emotion
-
-        # Play audio based on the current emotion
-        if current_emotion == "happy":
-            if st.session_state.playing != current_emotion:
-                st.session_state.playing = current_emotion
-                encoded_happy = base64.b64encode(happy).decode('utf-8')
-                audio.markdown('<audio  style="width: 100%;" src="data:audio/mp3;base64,{}" autoplay controls></audio>'.format(encoded_happy), unsafe_allow_html=True)
-        elif current_emotion == "sad":
-            if st.session_state.playing != current_emotion:
-                st.session_state.playing = current_emotion
-                encoded_sad = base64.b64encode(sad).decode('utf-8')
-                audio.markdown('<audio style="width: 100%;" src="data:audio/mp3;base64,{}" autoplay controls></audio>'.format(encoded_sad), unsafe_allow_html=True)
