c2cbcbcdda9ccf2e7cdc8a2ab8536dd875feddae
Dang Toan Bui <d.t.bui@se21.qmul.ac.uk>
2023-10-08 05:27:08 +0100

    Add way to get background images with background taken from image folder

--- app.py
+++ app.py
@@ -92,6 +92,29 @@ current = col2.empty()
 playing = col3.empty()
 audio = st.empty()  # Use empty() for dynamic content
 
+#set background images with background taken from image folder
+@st.cache_data
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('image/background.png')
+
 # Initialize these fields once to prevent them from disappearing
 detected.write("Detected emotion: `None`")
 current.write("Current emotion: `None`")
