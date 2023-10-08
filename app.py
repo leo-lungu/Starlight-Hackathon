e5203ce57c84445d71132e3f142bfcda834d4da1
Al Adnan Uddin <shuhanuddin6@gmail.com>
2023-10-08 04:13:11 +0100

    Fix the issue of current songs not being diplayed.

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ import tempfile
 
 from youtubeAPIHandler import youtube_search
 
-
-
-
+# Function to download audio from a YouTube URL
 def download_youtube_audio(url):
     yt = YouTube(url)
     audio_stream = yt.streams.filter(only_audio=True).first()
@@ -21,6 +19,7 @@ def download_youtube_audio(url):
     audio_stream.download(filename=audio_path)
     return audio_path
 
+# Function to extract URLs from a playlist text
 def extract_urls(playlist):
     urls = []
     for line in playlist:
@@ -30,56 +29,57 @@ def extract_urls(playlist):
             urls.append(url)
     return urls
 
-# Function to load a random mp3 file from a given folder
+# Function to load a random song file from a folder based on age group
 def load_random_song(folder, age_group):
     subfolder = "kids" if age_group in ['3-5', '6-10'] else "teens"
     folder_path = os.path.join(folder, subfolder)
     files = [f for f in os.listdir(folder_path) if f.endswith('.mp3')]
-    
     if not files:
         return None
-    
     random_file = random.choice(files)
+    # Update the current_song in the session state
+    st.session_state.current_song = random_file[:-4]
     return open(os.path.join(folder_path, random_file), "rb").read()
 
-# Query YouTube and generate a playlist
+# Function to query YouTube API and generate a playlist
 def generate_playlist(emotion, age):
     query = f"{emotion} songs for age {age}"
     playlist = youtube_search(query, max_results=20)
     return playlist
 
-
-# Initialize session state if not already done
+# Initialize Streamlit session state if not already initialized
 if "emotion" not in st.session_state:
     st.session_state.emotion = None
     st.session_state.emotions = queue.Queue()
     st.session_state.playing = None
     st.session_state.audio_player = None
-    st.session_state.scanning = False  # Add a scanning flag
+    st.session_state.scanning = False
+
+# Initialize current_song in session state
+if "current_song" not in st.session_state:
+    st.session_state.current_song = None
 
 # Import DeepFace for emotion recognition
 with st.spinner("Importing DeepFace..."):
     from deepface import DeepFace
 
-# Function to get the emotion from a given frame
+# Function to get emotion from a frame
 def get_emotion(frame):
     result = DeepFace.analyze(frame, actions=["emotion"], enforce_detection=False, silent=True)
     if isinstance(result, list) and len(result) > 0:
         emotion = result[0].get("dominant_emotion", None)
         if emotion:
             st.session_state.emotions.put(emotion)
-            # Maintain the queue length to 50
             if st.session_state.emotions.qsize() > 50:
                 st.session_state.emotions.get()
             return emotion
     return None
 
-# Function to get the current dominant emotion
+# Function to get the current dominant emotion from the session state
 def get_current_emotion():
     emotions = list(st.session_state.emotions.queue)
     if emotions:
         emotion = max(set(emotions), key=emotions.count)
-        # Minimum count for a dominant emotion to be valid
         if emotions.count(emotion) > 25:
             return emotion
     return None
@@ -92,79 +92,78 @@ current = col2.empty()
 playing = col3.empty()
 audio = st.empty()  # Use empty() for dynamic content
 
+# Initialize these fields once to prevent them from disappearing
+detected.write("Detected emotion: `None`")
+current.write("Current emotion: `None`")
+playing.write("Playing: `None`")
+
 # "Scan Again" button to restart scanning
 if st.button("Scan"):
     st.session_state.scanning = True  # Set the scanning flag to True to start scanning again
+    st.session_state.playing = None  # Reset the playing state to None
 
-# Adding the dropdown for age selection
+    # Reset these fields when scanning starts
+    detected.write("Detected emotion: `Scanning...`")
+    current.write("Current emotion: `Scanning...`")
+    playing.write("Playing: `None`")
+
+# Dropdown for age selection
 with col1:
     age = st.selectbox('Select Age Group', ['3-5', '6-10', '10-15', '15-20'])
 
-
-
-
-
-# Start the camera
+# Initialize the camera
 with st.spinner("Accessing Camera..."):
     camera = cv2.VideoCapture(0)
-with st.spinner("Starting Camera..."):
-    while True:
-        ret, frame = camera.read()
-        if ret is not None:
-            break
+while True:
+    ret, frame = camera.read()
+    if ret is not None:
+        break
 
-
-#Main loop for the camera feed
-while st.session_state.scanning:  # Continue scanning while the flag is True
+# Main loop for the camera feed and emotion detection
+while st.session_state.scanning:
     ret, frame = camera.read()
     if ret:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
         st.session_state.emotion = get_emotion(frame)
         current_emotion = get_current_emotion()
-
+        
         if current_emotion and (current_emotion != st.session_state.playing):
             st.session_state.playing = current_emotion
-
+            
+            # Code for handling online and offline music
             try:
                 playlist_text = generate_playlist(current_emotion, age=age)
                 urls = extract_urls(playlist_text)
                 random_song_url = random.choice(urls)
-                
+
                 if random_song_url:
                     audio_path = download_youtube_audio(random_song_url)
                     audio_data = open(audio_path, "rb").read()
-                    st.audio(audio_data, format="audio/webm")
-                
+                    encoded_song = base64.b64encode(audio_data).decode('utf-8')
+                    audio.markdown(f'<audio style="width: 100%;" src="data:audio/webm;base64,{encoded_song}" autoplay controls></audio>', unsafe_allow_html=True)
+                    st.session_state.current_song = YouTube(random_song_url).title
+
                 st.session_state.scanning = False
-                
+                st.session_state.playing = None  # Set playing to None until a new song is chosen
+
             except Exception as e:
-                st.write(f"Can't reach The internet, Offline songs Active.")
+                st.write(f"An error occurred: {e}. Playing local files.")
                 
                 # Pass the age group to the function
                 song_data = load_random_song(f"music/{current_emotion}", age)
                 if song_data:
                     encoded_song = base64.b64encode(song_data).decode('utf-8')
                     audio.markdown(f'<audio style="width: 100%;" src="data:audio/mp3;base64,{encoded_song}" autoplay controls></audio>', unsafe_allow_html=True)
-    
+
                 st.session_state.scanning = False
-                
-               
+                st.session_state.playing = None  # Set playing to None until a new song
 
         detected.write(f"Detected emotion: `{st.session_state.emotion}`")
         current.write(f"Current emotion: `{str(current_emotion)}`")
-        playing.write(f"Playing: `{str(st.session_state.playing)}`")
+        playing.write(f"Playing: `{str(st.session_state.current_song)}`")
 
-
-    
-
-
-
-# Release the camera when scanning is finished
+# Release the camera after scanning is complete
 camera.release()
-
-
-
-
+    
 
 
